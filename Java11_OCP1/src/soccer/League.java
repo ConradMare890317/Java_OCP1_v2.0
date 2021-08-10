b49bff33dfff933a318de1a0c933a80ea4cd3a38
ConradMare890317 <Conradmare@gmail.com>
2021-08-10 12:59:14 +0200

    2021/08/10 (pm)
    Classes created and populated used to create a new object + add.

--- Java11_OCP1/src/soccer/League.java
+++ Java11_OCP1/src/soccer/League.java
@@ -15,21 +15,22 @@ public class League {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        // Create team1      
         Player player1 = new Player();
         player1.playerName = "George Eliot";
         Player player2 = new Player();
         player2.playerName = "Graham Greene";
         Player player3 = new Player();
         player3.playerName = "Geoffrey Chaucer";
-        Player[] thePlayers = { player1, player2, player3 };
+        Player[] thePlayers = {player1, player2, player3 };
         
         Team team1 = new Team();
         team1.teamName = "The Greens";
         team1.playerArray = thePlayers;
         
-        //player1.playerName = "Robert Service";
         
+        // Create team2
         Team team2 = new Team();
         team2.teamName = "The Reds";
         team2.playerArray = new Player[3];
@@ -40,13 +41,26 @@ public class League {
         team2.playerArray[2] = new Player();
         team2.playerArray[2].playerName = "Rafael Sabatini";
         
-        for(Player thePlayer: team1.playerArray) {
-            System.out.println(thePlayer.playerName);
-        }
+        /* Practice 6-2. Remove the two for loops below */
+
         
-        for(Player thePlayer: team2.playerArray) {
-            System.out.println(thePlayer.playerName);
-        }
-    }
-    
+        /* Practice 6-2. Create a Game here */
+        Game currGame = new Game();
+        currGame.homeTeam = team1;
+        currGame.awayTeam = team2;
+        /* Practice 6-2. Create a Goal object here */
+        Goal goal1 = new Goal();
+        goal1.thePlayer = currGame.homeTeam.playerArray[2];
+        goal1.theTeam = currGame.homeTeam;
+        goal1.theTime = 55;
+        /* Practice 6-2. Put  Goal object in a Goal array and assign Goal array to goals attribute of Game object */
+        Goal[] theGoals = {goal1};
+        currGame.goals = theGoals;
+        /* Practice 6-2. Print out the score of the Game */
+        System.out.println("Goal scored after " + 
+                currGame.goals[0].theTime + " mins by " +
+                currGame.goals[0].thePlayer.playerName + " of " +
+                currGame.goals[0].theTeam.teamName);
+        
+    }   
 }
