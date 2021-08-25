4b576291936824406e8e391616c3b52f7a1657c1
ConradMare890317 <Conradmare@gmail.com>
2021-08-25 13:34:48 +0200

    2021/08/25 (pm)
    Handling Exceptions.

--- Java11_OCP1/src/soccer/League.java
+++ Java11_OCP1/src/soccer/League.java
@@ -12,6 +12,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.StringTokenizer;
 import utility.PlayerDatabase;
+import utility.PlayerDatabaseException;
 
 /**
  *
@@ -27,9 +28,12 @@ public class League {
         League theLeague = new League();
         
         /* Practice 14-1, Step 3a. Start a try block here */
+        try {
+            
+        
 
         /* Practice 14-1, Step 2a. Modify the line below to add more teams and players */
-        Team[] theTeams = theLeague.createTeams("The Robins,The Crows,The Swallows", 5);
+        Team[] theTeams = theLeague.createTeams("The Robins,The Crows,The Swallows, The Owls", 11);
         Game[] theGames = theLeague.createGames(theTeams);
 
         System.out.println(theLeague.getLeagueAnnouncement(theGames));
@@ -43,8 +47,11 @@ public class League {
         theLeague.showBestPlayers(theTeams);
         
         /* Practice 14-1, Step 3b. Close the try block here */
-        
+        }
         /* Practice 14-1, Step 3d. Add a catch block here */
+        catch (Exception e) {
+            e.printStackTrace(System.err);
+        }
 
     }
 
