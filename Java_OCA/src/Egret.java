0a870c4aab221f5eaefaecc23ae706449215b529
ConradMare890317 <Conradmare@gmail.com>
2021-09-01 08:47:03 +0200

    2021/09/01 (am)
    Oracle.Java_OCA exam prep book.

---
+++ Java_OCA/src/Egret.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Conrad
+ */
+
+/*
+Java_OCA book.
+Question12 - page38.
+*/
+public class Egret {
+    private String color;
+    public Egret() {
+        this("white");
+    }
+    
+    public Egret(String color) {
+        color = color;
+    }
+ 
+    public static void main(String[] args) {
+        Egret e = new Egret();
+        System.out.println("Color:" + e.color);
+    }
+ }
+ 
