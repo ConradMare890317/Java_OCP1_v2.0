01ceedc53e51942b43fd859b2cddcf4100ac8543
ConradMare890317 <Conradmare@gmail.com>
2021-09-02 19:15:17 +0200

    2021/09/02 (pm)
    Ch1 - ( if + double ) : Examples.

---
+++ Java_OCP1_HerbertSchildt/src/OCA/FindAllTadPole.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OCA;
+
+/**
+ *
+ * @author Conrad
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+interface CanSwim {
+}
+
+class Amphibian implements CanSwim {
+}
+
+class Tadpole extends Amphibian {
+}
+
+public class FindAllTadPole {
+    public static void main(String[] args) {
+        List<Tadpole> tadpoles = new ArrayList<Tadpole>();
+        for (Amphibian amphibian : tadpoles) {
+            __ tadpole = amphibian;
+        }
+    }
+}
