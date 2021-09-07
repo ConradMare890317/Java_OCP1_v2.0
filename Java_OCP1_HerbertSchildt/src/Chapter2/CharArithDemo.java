a3fa100819efeb22f4243e7303c315925a8dec72
ConradMare890317 <Conradmare@gmail.com>
2021-09-07 18:29:25 +0200

    2021/09/07 (pm)
    Floating points, Charcters,  Boolean Types,.

---
+++ Java_OCP1_HerbertSchildt/src/Chapter2/CharArithDemo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chapter2;
+
+/**
+ *
+ * @author Conrad
+ */
+public class CharArithDemo {
+    public static void main(String[] args) {
+        char ch;
+        
+        ch = 'X';
+        System.out.println("ch contains " + ch);
+        
+        ch++; // increment ch (A char can be incremented)
+        System.out.println("ch is now " + ch);
+        
+        ch = 90; // give ch the value of Z
+        System.out.println("ch is now " + ch);
+    }
+}
