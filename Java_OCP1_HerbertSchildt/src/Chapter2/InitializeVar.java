df2d643ef7ab7320afea2c6ddaff901d2fed1c53
ConradMare890317 <Conradmare@gmail.com>
2021-09-08 16:18:04 +0200

    2021/09/08 (pm)
    type var = value;

---
+++ Java_OCP1_HerbertSchildt/src/Chapter2/InitializeVar.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chapter2;
+
+/**
+ *
+ * @author Conrad
+ */
+public class InitializeVar {
+    public static void main(String[] args) {
+        int count = 10; // give count initial value of 10
+        char ch = 'X'; // initialize ch with letter X
+        float f = 1.2F; // f is initialized with 1.2
+        
+        // Declaring two or more values with a comma = one or more intial value
+        int a, b = 8, c = 19, d; // b and c have initializations
+    }
+}
