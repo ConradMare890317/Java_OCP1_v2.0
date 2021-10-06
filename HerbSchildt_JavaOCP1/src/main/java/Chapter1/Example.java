4204241d103145e70c740c26cbf8f35d3f93280e
Conrad <71630485+ConradMare890317@users.noreply.github.com>
2021-10-06 09:44:59 +0200

    2021/10/06 (am)
    Basic java ( Main, int/double etc ).

---
+++ HerbSchildt_JavaOCP1/src/main/java/Chapter1/Example.java
@@ -0,0 +1,7 @@
+package Chapter1;
+
+class Example {
+    public static void main(String args[]) {
+        System.out.println("Java drives the web.");
+    }
+}
---
+++ HerbSchildt_JavaOCP1/src/main/java/Chapter1/Example2.java
@@ -0,0 +1,17 @@
+package Chapter1;
+
+class Example2 {
+    public static void main(String args[]) {
+        int var1;
+        int var2;
+
+        var1 = 12345;
+
+        System.out.println("Var 1: " + var1);
+
+        var2 = var1 / 2;
+
+        System.out.print("Var 2 contains var1/2: ");
+        System.out.println(var2);
+    }
+}
