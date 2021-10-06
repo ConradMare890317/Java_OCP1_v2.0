ee1457f96a1449b8210f865ea9c758837220eff7
Conrad <71630485+ConradMare890317@users.noreply.github.com>
2021-10-06 11:01:59 +0200

    2021/10/06 (am)
    Statements, loops.

---
+++ HerbSchildt_JavaOCP1/src/main/java/Chapter1/ForDemo.java
@@ -0,0 +1,12 @@
+package Chapter1;
+
+class ForDemo {
+    public static void main(String args[]) {
+        int count;
+
+        for(count = 0; count < 5; count = count+1)
+            System.out.println("Count is: " + count);
+
+        System.out.println("Done!");
+    }
+}
---
+++ HerbSchildt_JavaOCP1/src/main/java/Chapter1/GalToLit.java
@@ -0,0 +1,18 @@
+package Chapter1;
+
+class GalToLit {
+    public static void main(String args[]) {
+        double gallons;
+        double liters;
+
+        gallons = 10;
+        liters = gallons * 3.7854;
+
+        System.out.println(gallons + " gallons is " + liters + " liters.");
+    }
+}
+
+/*
+Compile with: javac GalToLit.java
+Run with: java GalToLit
+ */
