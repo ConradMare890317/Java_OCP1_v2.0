49f6596f1bf75b2b5a4577bc629b32fb3701e7e7
Conrad <71630485+ConradMare890317@users.noreply.github.com>
2021-11-25 11:36:16 +0000

    2021/11/25 (am)
    Use Break as a Goto.

--- HerbSchildt_JavaOCP1/src/main/java/BreakLoop/BreakDemo.java
+++ HerbSchildt_JavaOCP1/src/main/java/BreakLoop/BreakDemo.java
@@ -2,6 +2,14 @@ package BreakLoop;
 
 public class BreakDemo {
     public static void main(String[] args) {
-        
+        int num;
+
+        num = 100;
+
+        for(int i = 0; i < num; i++) {
+            if (i*i >= num) break;
+            System.out.print(i + " ");
+        }
+        System.out.println("Loop complete.");
     }
 }
