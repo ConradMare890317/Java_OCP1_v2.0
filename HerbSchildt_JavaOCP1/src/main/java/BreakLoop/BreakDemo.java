4dc69b305f9ec32fe33b02cf166c3e2815f95949
Conrad <71630485+ConradMare890317@users.noreply.github.com>
2021-11-25 10:18:14 +0000

    2021/11/25 (am)
    Loops continued.

---
+++ HerbSchildt_JavaOCP1/src/main/java/BreakLoop/BreakDemo.java
@@ -0,0 +1,7 @@
+package BreakLoop;
+
+public class BreakDemo {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ HerbSchildt_JavaOCP1/src/main/java/DoWhileLoop/DWDemo.java
@@ -0,0 +1,14 @@
+package DoWhileLoop;
+
+public class DWDemo {
+    public static void main(String[] args)
+        throws java.io.IOException {
+
+        char ch;
+
+        do {
+            System.out.print("Press a key followed by enter: ");
+            ch = (char) System.in.read();
+        } while (ch != 'q');
+    }
+}
