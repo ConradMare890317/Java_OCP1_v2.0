7983972ed4fe69cb39509db5a5aed3a2a345c69e
Conrad <71630485+ConradMare890317@users.noreply.github.com>
2021-11-26 17:47:54 +0000

    2021/11/26 (am)

---
+++ HerbSchildt_JavaOCP1/src/main/java/BreakLoop/ContDemo.java
@@ -0,0 +1,13 @@
+package BreakLoop;
+
+public class ContDemo {
+    public static void main(String[] args) {
+        int i;
+
+        // print even numbers between 0 and 100
+        for (i = 0; i <= 100; i++) {
+            if ((i%2) != 0);
+            System.out.println(i);
+        }
+    }
+}
---
+++ HerbSchildt_JavaOCP1/src/main/java/BreakLoop/ContToLabel.java
@@ -0,0 +1,12 @@
+package BreakLoop;
+
+public class ContToLabel {
+    public static void main(String[] args) {
+
+        outerloop:
+        for (int i = 1; i < 10; i++) {
+            System.out.print("\nOuter loop pass " + i + ", Inner loop: ");
+            for (int j)
+        }
+    }
+}
