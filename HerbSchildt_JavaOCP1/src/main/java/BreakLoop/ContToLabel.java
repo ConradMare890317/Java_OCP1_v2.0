894546bc7950271077be9105ca25c7e710cf47aa
Conrad <71630485+ConradMare890317@users.noreply.github.com>
2021-11-28 18:24:15 +0000

    2021/11/28 (pm)
    Continue.

--- HerbSchildt_JavaOCP1/src/main/java/BreakLoop/ContToLabel.java
+++ HerbSchildt_JavaOCP1/src/main/java/BreakLoop/ContToLabel.java
@@ -6,7 +6,10 @@ public class ContToLabel {
         outerloop:
         for (int i = 1; i < 10; i++) {
             System.out.print("\nOuter loop pass " + i + ", Inner loop: ");
-            for (int j)
+            for (int j = 1; j < 10; j++) {
+                if (j == 5) continue outerloop;
+                System.out.print(j);
+            }
         }
     }
 }
