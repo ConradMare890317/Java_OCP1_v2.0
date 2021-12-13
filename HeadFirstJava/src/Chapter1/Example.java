2d32dbfead4965e38e795c52f61f77f57f25ac59
Conrad <71630485+ConradMare890317@users.noreply.github.com>
2021-12-13 21:15:33 +0000

    2021/12/13 (pm)
    "Head first Java" book.

---
+++ HeadFirstJava/src/Chapter1/Example.java
@@ -0,0 +1,29 @@
+package Chapter1;
+
+public class Example {
+    public static void main(String[] args) {
+        int size = 27;
+        String name = "Frido";
+        Dog, myDog = new Dog(name, size);
+        x = size - 5;
+        if (x < 15) myDog.bark(8);
+
+        while (x > 3) {
+            myDog.play();
+        }
+
+        int [] num = {2,4,6,8};
+        System.out.print("Hello");
+        System.out.print("Dog: " + name);
+        String num = "8";
+        int z = Integer.parseInt(num);
+
+        try {
+            readTheFile(myFile.txt);
+        }
+
+        catch (FileNotFoundException ex) {
+            System.out.println("File not found.");
+        }
+    }
+}
