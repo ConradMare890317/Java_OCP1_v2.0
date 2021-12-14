57c8dca9949c9ad0b04ed6d56fd3144bf93093ac
Conrad <71630485+ConradMare890317@users.noreply.github.com>
2021-12-14 17:04:34 +0000

    2021/12/14 (pm)
    "Head First Java..." - Study guide 2.

--- HeadFirstJava/src/Chapter1/AnatomyOfClass.java
+++
@@ -1,7 +0,0 @@
-package Chapter1;
-
-public class AnatomyOfClass {
-    public static void main(String[] args) {
-        System.out.print("Good-bye blue sky.");
-    }
-}
--- HeadFirstJava/src/Chapter1/Example.java
+++ HeadFirstJava/src/Chapter1/Example.java
@@ -3,25 +3,28 @@ package Chapter1;
 public class Example {
     public static void main(String[] args) {
         int size = 27;
-        String name = "Frido";
-        Dog, myDog = new Dog(name, size);
+        String Fido;
+        String name = "Fido";
+
+        Dog myDog = new Dog(name, size);
+
         x = size - 5;
         if (x < 15) myDog.bark(8);
-
         while (x > 3) {
             myDog.play();
         }
 
-        int [] num = {2,4,6,8};
-        System.out.print("Hello");
-        System.out.print("Dog: " + name);
-        String num = "8";
-        int z = Integer.parseInt(num);
+        int[] numList = {2, 4, 6, 8};
 
+        System.out.print("Hello");
+
+        System.out.print("Dog: " + name);
+
+        String num = "8";
+        int z = Integer.parseInt("num");
         try {
             readTheFile(myFile.txt);
         }
-
         catch (FileNotFoundException ex) {
             System.out.println("File not found.");
         }
